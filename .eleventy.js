bcc2f85076eac840482fcba058690f6966f0d3b0
developmedaddy <developmedaddy@aliensz.org>
2022-10-30 18:57:04 +0100

    Change body font and color, move disclaimer to footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,31 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+const passthroughFiles = ['src/robots.txt',
+                          'src/robots.txt',
+                          'src/assets/fonts',
+                          'src/assets/img',
+                          'src/assets/pdf',
+                          'src/assets/styles.css',];
+
+
 module.exports = config => {
 
-  // config.addCollection('da', collectionApi => collectionApi.getFilteredByGlob('./src/content/da/*'));
+  // Plugins
+  config.addPlugin(eleventyNavigationPlugin);
+
+  // Turn offf autoescaping
+  config.setNunjucksEnvironmentOptions({
+    autoescape: false
+  });
+
+  /* config.addCollection('da', collectionApi => collectionApi.getFilteredByGlob('./src/content/da/*')); */
   config.addCollection('en', collectionApi => collectionApi.getFilteredByGlob('./src/content/en/*'));
   config.addCollection('es', collectionApi => collectionApi.getFilteredByGlob('./src/content/es/*'));
   config.addCollection('en_articles', collectionApi => collectionApi.getFilteredByGlob('./src/content/en/articles/*'));
   config.addCollection('es_articles', collectionApi => collectionApi.getFilteredByGlob('./src/content/es/articles/*'));
 
-  config.setNunjucksEnvironmentOptions({
-    autoescape: false
-  });
+  passthroughFiles.forEach(element => config.addPassthroughCopy(element));
 
-  config.addPlugin(eleventyNavigationPlugin);
-
-  // These files get "passed through" to the build
-  config.addPassthroughCopy('src/assets/fonts');
-  config.addPassthroughCopy('src/assets/img');
-  config.addPassthroughCopy('src/assets/pdf');
-  config.addPassthroughCopy('src/assets/styles.css');
   return {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
