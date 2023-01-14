d7d834a6fb8e73f63df664028c81fa2691c9df4d
developmedaddy <developmedaddy@aliensz.org>
2023-01-14 17:05:34 +0100

    Add english journal club page

---
+++ src/_data/club_schedule.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    "date": "XX/02/22",
+    "topic_en": "Hip replacement",
+    "topic_es": "...",
+    "archived": false,
+  },
+  {
+    "date": "XX/03/22",
+    "topic_en": "Coronary artery disease",
+    "topic_es": "...",
+    "archived": false,
+  },
+  {
+    "date": "XX/04/22",
+    "topic_en": "Migraine",
+    "topic_es": "...",
+    "archived": false,
+  },
+  {
+    "date": "XX/05/22",
+    "topic_en": "Achilles tendinopathy",
+    "topic_es": "...",
+    "archived": false,
+  },
+];
