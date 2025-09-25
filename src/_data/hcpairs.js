b439dc2bb85c3ebf1498e7f74cc785e4dabe4178
fkkehlet <developmedaddy@aliensz.org>
2025-09-25 17:05:25 +0200

    add two forms

---
+++ src/_data/hcpairs.js
@@ -0,0 +1,80 @@
+module.exports = [
+  {
+    statement: 'Puede aún esperarse que los pacientes con dolor lumbar crónico cumplan con todas sus actividades laborales y familiares a pesar de su dolor.',
+    inverted: true,
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Un aumento en el dolor es un indicador de que los pacientes con dolor lumbar crónico deben parar lo que estén haciendo hasta que el dolor disminuya.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: ' Los pacientes con dolor lumbar crónico no pueden realizar las actividades de su vida diaria cuando están con dolor.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Si su dolor desapareciera, los pacientes con dolor lumbar crónico volverían a ser tan activos como solían serlo.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Los pacientes con dolor lumbar crónico deberían tener los mismos beneficios que los discapacitados debido a su problema de dolor crónico.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Los pacientes con dolor lumbar crónico deben obligarse por sí mismos y por aquellos que les rodean a llevar a cabo las actividades cotidianas aunque su dolor sea grande.',
+    inverted: true,
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'La mayoría de gente espera demasiado de los pacientes con dolor lumbar crónico, dado su dolor.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Los pacientes con dolor lumbar crónico deben tener cuidado de no hacer nada que pueda empeorar su dolor.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Mientras tengan dolor, los pacientes con dolor lumbar crónico nunca podrán vivir tan bien como vivían antes.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Cuando su dolor empeora, los pacientes con dolor lumbar crónico encuentran muy difícil concentrarse en otra cosa.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Los pacientes con dolor lumbar crónico tienen que aceptar que son personas discapacitadas, debido a su dolor crónico.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'No hay ninguna manera de que los pacientes con dolor lumbar crónico puedan volver a hacer las cosas que solían hacer hasta que primero encuentren una cura para su dolor.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Los pacientes con dolor lumbar crónico frecuentemente se encuentran pensando en su dolor y en lo que éste ha hecho con sus vidas.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Aunque su dolor siempre esté ahí, los pacientes con dolor lumbar crónico a menudo no se dan cuenta de él mientras se mantienen ocupados.',
+    inverted: true,
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+  {
+    statement: 'Todos los problemas de los pacientes con dolor lumbar crónico se solucionarían si su dolor desapareciera.',
+    dataFirstYear: [1, 2, 3, 4, 5, 6, 7],
+    dataLastYear: [1, 2, 3, 4, 5, 6, 7],
+  },
+]
---
+++ src/_data/rnpq.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    statement: 'Es posible sentir dolor y no darse cuenta.',
+    correctAnswer: 'falso',
+    correctFirstYear: 27,
+    correctLastYear: 14,
+  },
+  {
+    statement: 'Cuando una parte de su cuerpo está lesionada, unos receptores especiales del dolor transmiten el mensaje de dolor al cerebro.',
+    correctAnswer: 'falso',
+    correctFirstYear: 2,
+    correctLastYear: 4,
+  },
+  {
+    statement: 'El dolor sólo se produce cuando usted se lesiona o corre el riesgo de lesionarse.',
+    correctAnswer: 'falso',
+    correctFirstYear: 88,
+    correctLastYear: 41,
+  },
+  {
+    statement: 'Cuando usted se lesiona, unos receptores especiales transmiten el mensaje de ‘peligro’ a su médula espinal.',
+    correctAnswer: 'verdadero',
+    correctFirstYear: 71,
+    correctLastYear: 34,
+  },
+  {
+    statement: 'Unos nervios especiales en su médula espinal transmiten mensajes de ‘peligro’ a su cerebro.',
+    correctAnswer: 'verdadero',
+    correctFirstYear: 77,
+    correctLastYear: 33,
+  },
+  {
+    statement: 'Los nervios se adaptan aumentando su nivel de excitación en reposo.',
+    correctAnswer: 'verdadero',
+    correctFirstYear: 31,
+    correctLastYear: 16,
+  },
+  {
+    statement: 'El dolor crónico indica que una lesión no se ha curado completamente.',
+    correctAnswer: 'falso',
+    correctFirstYear: 38,
+    correctLastYear: 33,
+  },
+  {
+    statement: 'Las lesiones más graves siempre causan dolor más intenso.',
+    correctAnswer: 'falso',
+    correctFirstYear: 85,
+    correctLastYear: 38,
+  },
+  {
+    statement: 'Las neuronas descendentes son siempre inhibitorias.',
+    correctAnswer: 'falso',
+    correctFirstYear: 36,
+    correctLastYear: 30,
+  },
+  {
+    statement: 'El dolor se produce cuando usted se lesiona.',
+    correctAnswer: 'falso',
+    correctFirstYear: 64,
+    correctLastYear: 35,
+  },
+  {
+    statement: 'Cuando usted se lesiona, el entorno en el que usted se encuentra no afectará a la cantidad de dolor que experimente, siempre y cuando la lesión sea exactamente la misma.',
+    correctAnswer: 'falso',
+    correctFirstYear: 85,
+    correctLastYear: 40,
+  },
+  {
+    statement: 'El cerebro decide cuando usted experimentará dolor.',
+    correctAnswer: 'verdadero',
+    correctFirstYear: 39,
+    correctLastYear: 28,
+  },
+]
